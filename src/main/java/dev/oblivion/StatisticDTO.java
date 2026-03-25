3ae6e9d6ad65cd1d3a1395793a46b27a1abaf138
dias-oblivion <gabrieldias335@gmail.com>
2026-03-25 12:03:06 -0300

    feat: creating basic application entities

---
+++ src/main/java/dev/oblivion/StatisticDTO.java
@@ -0,0 +1,20 @@
+package dev.oblivion;
+
+import lombok.Getter;
+
+@Getter
+public class StatisticDTO {
+    private long count;
+    private double avg;
+    private double max;
+    private double min;
+    private double sum;
+
+    public StatisticDTO(long count, double avg, double max, double min, double sum) {
+        this.count = count;
+        this.avg = avg;
+        this.max = max;
+        this.min = min;
+        this.sum = sum;
+    }
+}
---
+++ src/main/java/dev/oblivion/Transaction.java
@@ -0,0 +1,18 @@
+package dev.oblivion;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+
+        private BigDecimal value;
+        private OffsetDateTime date;
+
+}
